ca1d9bf46826f7e3e0ebc0e0b9fa1bfaf3375fd2
Martin Leopold <m@martinleopold.com>
2019-03-05 18:33:02 +0100

    Bundle config

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import multiEntry from 'rollup-plugin-multi-entry';
+import buble from 'rollup-plugin-buble';
+import { terser } from 'rollup-plugin-terser';
+
+const bubleConfig = {
+  transforms: { forOf: false } // See: https://buble.surge.sh/guide/#unsupported-features
+};
+
+export default {
+  input: [
+    'node_modules/three/build/three.js',
+    'node_modules/three/examples/js/controls/OrbitControls.js',
+    // 'node_modules/three/examples/js/exporters/OBJExporter.js',
+    'node_modules/dat.gui/build/dat.gui.js',
+    'app/main.js'
+  ],
+  context: 'window', // Replace global `this` with `window` instead of `undefined`
+  output: {
+    file: 'app/main.bundle.js',
+    format: 'iife',
+    name: 'app'
+  },
+  plugins: [ multiEntry(), buble(bubleConfig), terser() ]
+};
