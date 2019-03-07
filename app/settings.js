78804bedcee32fbd3aa3405f24b88c1f91f3604c
Martin Leopold <m@martinleopold.com>
2019-03-07 11:18:39 +0100

    Updated settings for client video

--- app/settings.js
+++ app/settings.js
@@ -20,24 +20,114 @@ COLORS SEASON 2019/20
 
 */
 
+
+// //Saved season 2019/20 still
+// {
+//   "version": 5,
+//   "timestamp": "2019-02-05T08:44:56.826Z",
+//   "camera": {
+//     "position": {
+//       "x": -24.099515319316733,
+//       "y": -22.85538454667371,
+//       "z": -3.89859423105204
+//     },
+//     "rotation": {
+//       "_x": 0.6112819370444281,
+//       "_y": -0.1768501186098347,
+//       "_z": 0.12267743719819832,
+//       "_order": "XYZ"
+//     },
+//     "fov": 75,
+//     "zoom": 1,
+//     "near": 0.01,
+//     "far": 1000
+//   },
+//   "config": {
+//     "W": 1280,
+//     "H": 800,
+//   },
+//   "params": {
+//     "bgColor": "#ffffff",
+//     "wireframeColor": "#d45156",
+//     "banner_options": {
+//       "length": 100,
+//       "height": 8.5,
+//       "segment_detail": 10.5,
+//       "segment_proportion": 2,
+//       "noise_heading": {
+//         "seed": 93,
+//         "freq": 0.1,
+//         "amp": 1.47,
+//         "octaves": 1,
+//         "persistence": 0.5
+//       },
+//       "noise_pitch": {
+//         "seed": 222,
+//         "freq": 0.2,
+//         "amp": 0.06,
+//         "octaves": 1,
+//         "persistence": 0.5
+//       },
+//       "noise_roll": {
+//         "seed": 333,
+//         "freq": 0.2,
+//         "amp": 0.63,
+//         "octaves": 1,
+//         "persistence": 0.5
+//       },
+//       "noise_displacement": {
+//         "seed": 0,
+//         "freq": 0.59,
+//         "amp": 0.6,
+//         "octaves": 4,
+//         "persistence": 0.26
+//       }
+//     },
+//     "shading": {
+//       "colors": [
+//         "#ff5959",
+//         "#ffea3b",
+//         "#ed1c24",
+//         "#c83f80"
+//       ],
+//       "emissiveIntesity": 0.87,
+//       "diffuseIntesity": 0.5700000000000001,
+//       "lambertStrength": 0.66,
+//       "lambertHarshness": 0.05,
+//       "flatShading": true,
+//       "scaleX": 0.28,
+//       "scaleY": 0.46,
+//       "rotate": 0,
+//       "translateX": 0,
+//       "translateY": 0
+//     }
+//   }
+// }
+
+
+
+
+
+
+
 export let settings =
 
 /* PASTE CONTENTS OF SETTINGS FILE BELOW */
 /* ------------------------------------- */
 
 {
-  "version": 5,
-  "timestamp": "2019-02-05T08:44:56.826Z",
+  "version": 4,
+  "timestamp": "2019-03-07T10:13:30.711Z",
   "camera": {
     "position": {
-      "x": -24.099515319316733,
-      "y": -22.85538454667371,
-      "z": -3.89859423105204
+      "x": -5.422728371949674,
+      "y": 1.751659254994279,
+      "z": 0.3470224885025728
     },
     "rotation": {
-      "_x": 0.6112819370444281,
-      "_y": -0.1768501186098347,
-      "_z": 0.12267743719819832,
+      "_x": -1.67472420911212,
+      "_y": -0.8478145381419262,
+      "_z": -1.7090113699543292,
       "_order": "XYZ"
     },
     "fov": 75,
@@ -48,10 +138,10 @@ export let settings =
   "config": {
     "W": 1280,
     "H": 800,
+    "EXPORT_TILES": 4
   },
   "params": {
     "bgColor": "#ffffff",
-    "wireframeColor": "#d45156",
     "banner_options": {
       "length": 100,
       "height": 8.5,
@@ -60,7 +150,7 @@ export let settings =
       "noise_heading": {
         "seed": 93,
         "freq": 0.1,
-        "amp": 1.47,
+        "amp": 0.08,
         "octaves": 1,
         "persistence": 0.5
       },
@@ -74,7 +164,7 @@ export let settings =
       "noise_roll": {
         "seed": 333,
         "freq": 0.2,
-        "amp": 0.63,
+        "amp": 0.12,
         "octaves": 1,
         "persistence": 0.5
       },
@@ -103,7 +193,29 @@ export let settings =
       "rotate": 0,
       "translateX": 0,
       "translateY": 0
-    }
+    },
+    "show_plane": false,
+    "show_wireframe": false,
+    "show_anim": true,
+    "show_normals": false,
+    "show_path": true,
+    "show_axes": true,
+    "autoGenerate": false,
+    "animation": {
+      "time": 188.813995,
+      "fraction": 0.433,
+      "center": 1,
+      "speed": 1,
+      "pathAnimSpeed": 0.009000000000000001,
+      "pathDispSpeed": 0.1,
+      "pathDispAmp": 0.1,
+      "pathDispFreq": 0.1,
+      "camPos": 0.5,
+      "camLock": false,
+      "camDist": 2,
+      "distLock": false
+    },
+    "wireframeColor": "#d45156"
   }
 }
 
